4aa4259bf709cb27cb682791b1d127a47e3fcacc
Jakub Strebeyko <strebeyko@gmx.com>
2017-10-04 21:35:48 +0200

    font, script and stylin'

--- js/app.js
+++ js/app.js
@@ -959,6 +959,8 @@ module.exports = getActiveElement;
 "use strict";
 
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
 var _react = __webpack_require__(6);
 
 var _react2 = _interopRequireDefault(_react);
@@ -969,20 +971,64 @@ var _reactDom2 = _interopRequireDefault(_reactDom);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
 document.addEventListener('DOMContentLoaded', function () {
 
-    var numA = parseInt(prompt('Podaj liczbę A'));
-    var numB = parseInt(prompt('Podaj liczbę B'));
+    var name = prompt('Jak masz na imię?');
+
+    var Top = function (_React$Component) {
+        _inherits(Top, _React$Component);
+
+        function Top() {
+            _classCallCheck(this, Top);
+
+            return _possibleConstructorReturn(this, (Top.__proto__ || Object.getPrototypeOf(Top)).apply(this, arguments));
+        }
+
+        _createClass(Top, [{
+            key: 'render',
+            value: function render() {
+                return _react2.default.createElement(
+                    'div',
+                    { className: 'top' },
+                    _react2.default.createElement(
+                        'h1',
+                        null,
+                        ' Siema, ',
+                        this.props.name,
+                        ' !!'
+                    ),
+                    _react2.default.createElement(
+                        'h2',
+                        null,
+                        'Witam na mojej stronie.'
+                    )
+                );
+            }
+        }]);
+
+        return Top;
+    }(_react2.default.Component);
 
     _reactDom2.default.render(_react2.default.createElement(
         'div',
-        null,
+        { 'class': 'top' },
         _react2.default.createElement(
             'h1',
             null,
-            ' ',
-            numA + numB,
-            ' '
+            ' Siema, ',
+            name,
+            ' !!'
+        ),
+        _react2.default.createElement(
+            'h2',
+            null,
+            'Witam na mojej stronie.'
         )
     ), document.getElementById('app'));
 });
