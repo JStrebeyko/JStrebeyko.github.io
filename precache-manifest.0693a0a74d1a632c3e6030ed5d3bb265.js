b1e1c3f62e30e81bb9f504d0d4b326b0877d71a5
Jakub Strebeyko <strebeyko@gmx.com>
2018-10-16 00:31:01 +0200

    new, updated feel & look!

---
+++ precache-manifest.0693a0a74d1a632c3e6030ed5d3bb265.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "83ff2ee32db5f00d7925",
+    "url": "/js/chunk-vendors.bb3a6f6d.js"
+  },
+  {
+    "revision": "776faa6fad55dd27df42",
+    "url": "/js/app.59a6b1d2.js"
+  },
+  {
+    "revision": "baa343168992eac8dd0480e5b1ef9025",
+    "url": "/index.html"
+  },
+  {
+    "revision": "776faa6fad55dd27df42",
+    "url": "/css/app.7b3a694a.css"
+  }
+];
