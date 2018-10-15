e8a65cfa38be5251d53ad551b4ec7f5adc595cbb
Jakub Strebeyko <strebeyko@gmx.com>
2018-10-15 20:13:15 +0200

    deploy test

---
+++ docs/precache-manifest.034cf6179b4922752506f09edee06c26.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "83ff2ee32db5f00d7925",
+    "url": "/js/chunk-vendors.bb3a6f6d.js"
+  },
+  {
+    "revision": "a15dacdc3da39d3de027",
+    "url": "/js/app.80357c62.js"
+  },
+  {
+    "revision": "fb7d834762b54d6dfbfdfaaef72f1ae8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a15dacdc3da39d3de027",
+    "url": "/css/app.b27357b2.css"
+  }
+];
